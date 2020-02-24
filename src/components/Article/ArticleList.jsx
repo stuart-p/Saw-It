47db2f83ff2ddf5948f3451bd75cfeb1bd114a78
stuart-p <stuart_palmer85@hotmail.com>
2020-02-24 13:30:14 +0000

    basic folder structure in place

---
+++ src/components/Article/ArticleList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ArticleList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>first article item</li>
+      </ul>
+    );
+  }
+}
+
+export default ArticleList;
